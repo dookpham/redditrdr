8e041a176f09f051a285e134ab8be6594abba524
DookPham <dookpham@gmail.com>
2016-09-10 11:40:46 -0700

    Install Redux dependencies

---
+++ client/components/RedditView.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RedditView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+  }
+}
--- client/index.js
+++ client/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import RedditList from './components/RedditList.jsx';
+
+//get reddit homeview
+// const createComponentView = ()
+
+// const createComponent = function(child) {
+//   const data = child.data;
+//   const url = data.url;
+//   const title = data.title;
+//   const domain = data.domain;
+//   const ups = data.ups;
+//   //downs not accurate
+//   const img = data.preview.images[0].source; //height, url, width
+//   const author = data.author;
+//   const subreddit = data.subreddit;
+
+
+// }
+var redditData;
+
+const fetchHomeView = () => {
+  axios.get('http://www.reddit.com/.json')
+    .then(response => {
+      console.log(response.data.children);
+      redditData = response.data.children;
+      return response.data.children;
+    })
+}
+
+fetchHomeView()
 
 ReactDOM.render(
-  <h1>Reddit Rdr</h1>
+  <div>
+    <h1>Reddit Rdr</h1>
+    <RedditList redditData={redditData}/>
+  </div>
   , document.getElementById('app')
 );
